94542923539c4e014189a3dd5147ddcc1e28f1cd
Vikram Narayanan <vikram186@gmail.com>
2021-08-09 18:51:24 -0700

    d3log/dred: Add an accumulator that sends metadata as well

--- rust/template/d3log/src/dred.rs
+++ rust/template/d3log/src/dred.rs
@@ -5,13 +5,14 @@
 
 use crate::{Batch, Evaluator, FactSet, Port, Transport, ValueSet};
 use std::cell::RefCell;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 #[derive(Clone)]
 pub struct Dred {
     eval: Evaluator,
     out_port: Port,
-    accumulator: RefCell<ValueSet>,
+    accumulator: Arc<Mutex<ValueSet>>,
+    demo_accumulator: Arc<Mutex<Vec<Batch>>>,
 }
 
 unsafe impl Sync for Dred {}
@@ -21,7 +22,8 @@ impl Dred {
         let d = Dred {
             eval,
             out_port,
-            accumulator: RefCell::new(ValueSet::new()),
+            accumulator: Arc::new(Mutex::new(ValueSet::new())),
+            demo_accumulator: Arc::new(Mutex::new(Vec::new())),
         };
 
         (d.clone(), Arc::new(d))
@@ -30,14 +32,26 @@ impl Dred {
     pub fn close(&self) {
         let batch = Batch::new(
             FactSet::Empty(),
-            FactSet::Value(self.accumulator.borrow().clone()),
+            FactSet::Value(self.accumulator.lock().expect("lock").clone()),
         );
         self.dump_acc();
         self.out_port.send(batch);
     }
 
+    pub fn close_with_metadata(&self) {
+        for batch in self.demo_accumulator.lock().expect("lock").iter() {
+            let mut value_set = ValueSet::new();
+            for (r, v, mut w) in &ValueSet::from(&*self.eval, batch.clone().data).expect("iterator") {
+                w = -w;
+                value_set.insert(r, v, w);
+            }
+            let out_batch = Batch::new(batch.clone().meta, FactSet::Value(value_set));
+            self.out_port.send(out_batch.clone());
+        }
+    }
+
     fn dump_acc(&self) {
-        for (_r, v, w) in &*self.accumulator.borrow() {
+        for (_r, v, w) in &*self.accumulator.lock().expect("lock") {
             println!("v {} w {}", v, w);
         }
     }
@@ -48,8 +62,10 @@ impl Transport for Dred {
         for (r, v, mut w) in &ValueSet::from(&*self.eval, b.clone().data).expect("iterator") {
             // Invert the weight and add to the accumulator
             w = -w;
-            self.accumulator.borrow_mut().insert(r, v, w);
+            self.accumulator.lock().expect("lock").insert(r, v, w);
         }
+        // demo accumulator contains the metadata batch as well
+        self.demo_accumulator.lock().expect("lock").push(b.clone());
         // Send it out to the output port
         self.out_port.send(b);
     }
