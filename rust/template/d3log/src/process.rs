959cfbc7cd64f295f7cae4687fb0fa899f734acc
Eric Hoffman <ehoffman@vmware.com>
2021-08-16 14:17:56 -0700

    restructure process to clean up the flow control and resuse more code

--- rust/template/d3log/src/process.rs
+++ rust/template/d3log/src/process.rs
@@ -8,7 +8,7 @@
 use {
     crate::{
         async_error, broadcast::PubSub, error::Error, fact, function, json_framer::JsonFramer,
-        send_error, Batch, Evaluator, FactSet, Instance, Node, Port, RecordSet, Transport,
+        recfact, send_error, Batch, Evaluator, FactSet, Instance, Node, Port, RecordSet, Transport,
     },
     differential_datalog::record::*,
     nix::sys::signal::*,
@@ -24,6 +24,17 @@ use {
     tokio::{io::AsyncReadExt, io::AsyncWriteExt},
     tokio_fd::AsyncFd,
 };
+
+#[derive(Clone)]
+pub struct Child {
+    uuid: u128,
+    instance: Arc<Instance>,
+    pid: Option<Pid>,
+    w: isize,
+    management: bool,
+    last_announcement: Option<RecordSet>,
+    executable: String,
+}
 
 type Fd = std::os::unix::io::RawFd;
 
@@ -41,7 +52,6 @@ impl Transport for FileDescriptorPort {
         let afd = self.fd.clone();
         let e = self.instance.eval.clone();
 
-        // no vals today, check back tomorrow
         let b = Batch::new(
             FactSet::Record(RecordSet::from(e.clone(), b.clone().meta)),
             FactSet::Record(RecordSet::from(e.clone(), b.clone().data)),
@@ -68,127 +78,183 @@ where
     }
 }
 
-pub fn read_output_fact(fd: Fd, id: Node, instance: Arc<Instance>, p: Port, kind: String) {
-    let i2 = instance.clone();
-    instance.clone().rt.spawn(async move {
-        async_error!(
-            instance.eval.clone(),
-            read_output(fd, move |b: &[u8]| {
-                p.send(fact!(d3_application::TextStream,
-                         t => i2.clone().eval.now().into_record(),
-                         kind => kind.clone().into_record(),
-                         id => id.clone().into_record(),
-                         body => std::str::from_utf8(b).expect("").into_record()));
-            })
-            .await
-        );
-    });
-}
-
 #[derive(Clone)]
 pub struct ProcessInstance {
     instance: Arc<Instance>,
     new_evaluator: Arc<dyn Fn(Node, Port) -> Result<(Evaluator, Batch), Error> + Send + Sync>,
-    manager: Arc<Mutex<HashMap<Node, (isize, Option<Pid>)>>>,
-    processes: Arc<Mutex<HashMap<Pid, Arc<Mutex<Child>>>>>,
+    manager: Arc<Mutex<HashMap<Node, Child>>>,
 }
 
 impl Transport for ProcessInstance {
     fn send(&self, b: Batch) {
-        // we think the dispatcher has given only facts from our relation
-        // this should be from, is that not so?
         for (_, p, weight) in &RecordSet::from(self.instance.eval.clone(), b.data) {
             let uuid_record = p.get_struct_field("id").unwrap();
             let uuid = async_error!(self.instance.eval.clone(), Node::from_record(uuid_record));
 
             let mut manager = self.manager.lock().expect("lock");
-            let value = manager.entry(uuid).or_insert_with(|| (0, None));
-            // XXX: Consolidate weights manually?
-            let w = value.0 + weight;
-            let child_pid = value.1;
-            value.0 = w;
-            if w > 0 {
-                // Start instance if one is not already present
-                if child_pid.is_none() {
-                    let id = u128::from_record(p.get_struct_field("id").unwrap()).expect("target");
-                    let management = p.get_struct_field("management").is_some(); // isn't this a boolean from dd?
-                    let pid = self.make_child(p, id, management).expect("fork failure");
-                    value.1 = Some(pid);
+            // just use the child structure
+
+            let mut child = if let Some(exec) = p.get_struct_field("path") {
+                // FIXME: Temporary fix. we are using some UI value to_string that adds quotes
+                let executable = exec.to_string().replace("\"", "");
+                let management = p.get_struct_field("management").is_some(); // isn't this a boolean from dd?
+                manager.entry(uuid).or_insert_with(|| Child {
+                    instance: self.instance.clone(),
+                    uuid,
+                    management,
+                    pid: None,
+                    executable,
+                    w: 0,
+                    last_announcement: None,
+                })
+            } else {
+                panic!("process record format");
+            };
+
+            child.w += weight;
+
+            if child.w > 0 {
+                // Start instance if one is not already present .. what if one is present .. someone changed exec?
+                if child.pid.is_none() {
+                    child.start().expect("fork failure");
                 }
-            } else if w <= 0 {
-                // xxxx retract InstanceStatus!!
-                // what about other values of weight?
-                // kill if we can find the uuid..i guess and if the total weight is 1
-                if let Some(pid) = child_pid {
-                    // send SIGKILL to pid and wait for the child?
+            } else {
+                if let Some(pid) = child.pid {
+                    // wait for the child?
                     kill(pid, SIGKILL).expect("kill failed");
-                    // Update the value to None
-                    value.1 = None;
+                    child.pid = None;
+                    // lock?
+                    child.report_status();
                 }
             }
         }
     }
 }
 
-pub struct Child {
-    uuid: u128,
-    eval: Evaluator,
-    management: Port,
-}
-
 impl Child {
-    pub fn read_management(instance: Arc<Instance>) {
-        let instance_clone = instance.clone();
-        let instance_clone2 = instance.clone();
-        instance.rt.block_on(async move {
-            let management_from_parent =
-                instance_clone
-                    .broadcast
-                    .clone()
-                    .subscribe(Arc::new(FileDescriptorPort {
-                        instance: instance_clone.clone(),
-                        fd: Arc::new(AsyncMutex::new(
-                            AsyncFd::try_from(MANAGEMENT_OUTPUT_FD).expect("asyncfd"),
-                        )),
-                    }));
+    // both inputs in a parent child relation into batch sends
+    fn reader(instance: Arc<Instance>, infd: Fd, outfd: Fd) {
+        let i2 = instance.clone();
 
-            instance_clone.rt.spawn(async {
-                let instance_clone3 = instance_clone2.clone();
-                let mut jf = JsonFramer::new();
-                async_error!(
-                    instance_clone3.clone().eval.clone(),
-                    read_output(MANAGEMENT_INPUT_FD, move |b: &[u8]| {
-                        let x = async_error!(instance_clone2.clone().eval.clone(), jf.append(b));
-                        for i in x {
-                            let v = async_error!(
-                                instance_clone2.clone().eval.clone(),
-                                Batch::deserialize(i)
-                            );
-                            management_from_parent.clone().send(v);
-                        }
-                    })
-                    .await
-                );
-            });
+        let outp = Arc::new(FileDescriptorPort {
+            instance: i2.clone(),
+            fd: Arc::new(AsyncMutex::new(
+                AsyncFd::try_from(outfd).expect("async fd failed"),
+            )),
+        });
+
+        instance.clone().rt.spawn(async move {
+            let mut jf = JsonFramer::new();
+            let management_input = instance.broadcast.clone().subscribe(outp.clone());
+            let i3 = i2.clone();
+            async_error!(
+                i2.clone().eval.clone(),
+                read_output(infd, move |b: &[u8]| {
+                    let i4 = i3.clone();
+                    for i in async_error!(i3.clone().eval.clone(), jf.append(b)) {
+                        let b: Batch = async_error!(i4.clone().eval.clone(), Batch::deserialize(i));
+                        management_input.clone().send(b);
+                    }
+                })
+                .await
+            );
         });
     }
 
-    pub fn new(uuid: u128, _pid: Pid, instance: Arc<Instance>) -> Self {
-        Self {
-            eval: instance.eval.clone(),
-            uuid,
-            management: instance.broadcast.clone(),
+    pub fn read_management(instance: Arc<Instance>) {
+        Child::reader(instance, MANAGEMENT_INPUT_FD, MANAGEMENT_OUTPUT_FD);
+    }
+
+    pub fn read_output_fact(self, fd: Fd, kind: String) {
+        let i2 = self.instance.clone();
+        self.instance.clone().rt.spawn(async move {
+            let i3 = i2.clone();
+            let i4 = i2.clone();
+            async_error!(
+                i3.eval.clone(),
+                read_output(fd, move |b: &[u8]| {
+                    self.instance.broadcast.clone().send(fact!(d3_application::TextStream,
+                                                               t => i4.clone().eval.now().into_record(),
+                                                               kind => kind.clone().into_record(),
+                                                               id => self.uuid.clone().into_record(),
+                                                               body => std::str::from_utf8(b).expect("").into_record()));
+                })
+                .await
+            );
+        });
+    }
+
+    pub fn report_status(&mut self) {
+        if let Some(la) = self.last_announcement.clone() {
+            self.instance
+                .broadcast
+                .send(Batch::new(FactSet::Empty(), FactSet::Record(la.negate())));
+        }
+        if self.pid.is_some() {
+            let f = recfact!(
+                d3_application::InstanceStatus,
+                id => self.uuid.into_record(),
+                memory_bytes => 0.into_record(),
+                threads => 0.into_record(),
+                time => self.instance.eval.now().into_record());
+            self.last_announcement = Some(f.clone());
+            self.instance
+                .broadcast
+                .send(Batch::new(FactSet::Empty(), FactSet::Record(f.clone())));
         }
     }
 
-    // fix nega vs posi
-    pub fn report_status(&self) {
-        self.management.send(fact!(
-            d3_application::InstanceStatus,
-            id => self.uuid.into_record(),
-            memory_bytes => 0.into_record(),
-            threads => 0.into_record(),
-            time => self.eval.now().into_record()));
+    // this should manage a filesystem resident cache of executable images,
+    // potnetially addressed with a uuid or a url
+
+    pub fn start(&mut self) -> Result<(), Error> {
+        // ideally we wouldn't allocate the management pair
+        // unless we were actually going to use it..really we should have
+        // two input relations, one for d3log programs and one for other things
+
+        let (management_in_r, management_in_w) = pipe().unwrap();
+        let (management_out_r, management_out_w) = pipe().unwrap();
+
+        let (standard_in_r, _standard_in_w) = pipe().unwrap();
+        let (standard_out_r, standard_out_w) = pipe().unwrap();
+        let (standard_err_r, standard_err_w) = pipe().unwrap();
+
+        let s2 = self.clone();
+        match unsafe { fork() } {
+            Ok(ForkResult::Parent { child }) => {
+                if self.management {
+                    // instance status!
+                    Child::reader(s2.instance.clone(), management_out_r, management_in_w);
+                }
+                self.pid = Some(child);
+                s2.clone()
+                    .read_output_fact(standard_out_r, "stdout".to_string());
+                s2.clone()
+                    .read_output_fact(standard_err_r, "stderr".to_string());
+                Ok(())
+            }
+
+            Ok(ForkResult::Child) => {
+                if self.management {
+                    dup2(management_out_w, MANAGEMENT_OUTPUT_FD)?;
+                    dup2(management_in_r, MANAGEMENT_INPUT_FD)?;
+                }
+
+                dup2(standard_in_r, 0)?;
+                dup2(standard_out_w, 1)?;
+                dup2(standard_err_w, 2)?;
+
+                let e = CString::new(self.executable.clone()).expect("CString::new failed");
+                let env1 =
+                    CString::new(format!("uuid={}", self.uuid)).expect("CString::new failed");
+                let env2 = CString::new("RUST_BACKTRACE=1").expect("CString::new failed");
+                execve(&e.clone(), &[e.clone()], &[env1, env2])?;
+                panic!("exec failed?");
+            }
+            Err(_) => {
+                panic!("Fork failed!");
+            }
+        }
     }
 }
 
@@ -204,122 +270,7 @@ impl ProcessInstance {
                 instance: instance.clone(),
                 new_evaluator: new_evaluator.clone(),
                 manager: Arc::new(Mutex::new(HashMap::new())),
-                processes: Arc::new(Mutex::new(HashMap::new())),
             }),
         )
-    }
-
-    // arrange to listen to management channels if they exist
-    // this should manage a filesystem resident cache of executable images,
-    // potnetially addressed with a uuid or a url
-
-    // since this is really an async error maybe deliver it here
-    pub fn make_child(&self, process: Record, id: Node, management: bool) -> Result<Pid, Error> {
-        // ideally we wouldn't allocate the management pair
-        // unless we were actually going to use it..really we should have
-        // two input relations, one for d3log programs and one for other things
-
-        let (management_in_r, management_in_w) = pipe().unwrap();
-        let (management_out_r, management_out_w) = pipe().unwrap();
-
-        let (standard_in_r, _standard_in_w) = pipe().unwrap();
-        let (standard_out_r, standard_out_w) = pipe().unwrap();
-        let (standard_err_r, standard_err_w) = pipe().unwrap();
-
-        match unsafe { fork() } {
-            Ok(ForkResult::Parent { child }) => {
-                let child_obj = Arc::new(Mutex::new(Child::new(id, child, self.instance.clone())));
-
-                // oddly symmetric with Child::read_management
-                if management {
-                    let c2 = child_obj.clone();
-                    let i3 = self.instance.clone();
-                    self.instance.clone().rt.spawn(async move {
-                        let mut jf = JsonFramer::new();
-                        let mut first = true;
-
-                        let management_to_child = Arc::new(FileDescriptorPort {
-                            instance: i3.clone(),
-                            fd: Arc::new(AsyncMutex::new(
-                                AsyncFd::try_from(management_in_w).expect("async fd failed"),
-                            )),
-                        });
-
-                        let sh_management =
-                            i3.broadcast.clone().subscribe(management_to_child.clone());
-
-                        let i4 = i3.clone();
-                        async_error!(
-                            i3.eval.clone(),
-                            read_output(management_out_r, move |b: &[u8]| {
-                                for i in async_error!(i4.eval.clone(), jf.append(b)) {
-                                    let s = async_error!(i4.eval.clone(), std::str::from_utf8(&i));
-                                    let b: Batch =
-                                        async_error!(i4.eval.clone(), serde_json::from_str(s));
-                                    sh_management.clone().send(b);
-                                    if first {
-                                        c2.clone().lock().expect("lock").report_status();
-                                        first = false;
-                                    }
-                                }
-                            })
-                            .await
-                        );
-                    });
-                }
-
-                read_output_fact(
-                    standard_out_r,
-                    id,
-                    self.instance.clone(),
-                    self.instance.broadcast.clone(),
-                    "stdout".to_string(),
-                );
-                read_output_fact(
-                    standard_err_r,
-                    id,
-                    self.instance.clone(),
-                    self.instance.broadcast.clone(),
-                    "stderr".to_string(),
-                );
-
-                self.processes
-                    .lock()
-                    .expect("lock")
-                    .insert(child, child_obj);
-                Ok(child)
-            }
-
-            Ok(ForkResult::Child) => {
-                if process.get_struct_field("management").is_some() {
-                    dup2(management_out_w, MANAGEMENT_OUTPUT_FD)?;
-                    dup2(management_in_r, MANAGEMENT_INPUT_FD)?;
-                }
-
-                dup2(standard_in_r, 0)?;
-                dup2(standard_out_w, 1)?;
-                dup2(standard_err_w, 2)?;
-
-                if let Some(exec) = process.get_struct_field("path") {
-                    // FIXME: Temporary fix. this should be fixed ddlog-wide
-                    let exec = exec.to_string().replace("\"", "");
-                    if let Some(id) = process.get_struct_field("id") {
-                        let path = CString::new(exec.clone()).expect("CString::new failed");
-                        let arg0 = CString::new(exec).expect("CString::new failed");
-                        let u = format!("uuid={}", id);
-                        let env1 = CString::new(u).expect("CString::new failed");
-                        let env2 = CString::new("RUST_BACKTRACE=1").expect("CString::new failed");
-                        execve(&path, &[arg0], &[env1, env2])?;
-                    }
-                } else {
-                    return Err(Error::new("malformed process record".to_string()));
-                }
-                // XXX: should never reach
-                panic!("exec failed?");
-            }
-            Err(_) => {
-                panic!("Fork failed!");
-            }
-        }
     }
 }
