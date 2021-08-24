c0c515309220e8d9a83b9e84d8aa3efe7b54f21e
Eric Hoffman <ehoffman@vmware.com>
2021-08-24 09:30:21 -0700

    fix <some> ordering and multiplicity issues in display javascript

--- rust/template/d3log/src/display.js
+++ rust/template/d3log/src/display.js
@@ -8,8 +8,26 @@ var socket = false
 var connWait = 0
 var svgns = "http://www.w3.org/2000/svg"
 
-//                       x.addEventListener("click",
-//                                          function (evt) {putBatch(x.click)})
+function putBatch(k) {
+}
+
+function update(obj, prop, value, weight) {
+    if (prop in obj) {
+        weight += obj[prop][1]
+    }
+    console.log("update", obj, prop, value, weight);
+    obj[prop] = [value, weight];
+
+    if (svg in obj) {
+        if (weight > 0) {
+            push(obj, k, val);
+        }
+        // text is drawn as a separate pass w/ the associated object for stacking
+        if (weight > 0) {        
+            push_text(obj);
+        }
+    }    
+}
 
 function clear() {
     if (svg != false) {
@@ -27,72 +45,82 @@ function send(item) {
 
 var nodes = {}
 
-function push_text(obj, key, value) {
-    if ((key == "text") && (obj.svg)){
-        tobj = document.createElementNS(svgns, "text");
-        var textNode = document.createTextNode(value)
-        tobj.setAttributeNS(null,"x",obj.x);
-        tobj.setAttributeNS(null,"y",obj.y);
-        tobj.setAttributeNS(null,"text-anchor","middle");
-        tobj.setAttributeNS(null,"font-size","24");
-        obj.textobj = tobj;
-        tobj.appendChild(textNode);
-        svg.appendChild(tobj);
+function push_text(obj) {
+    if ("text" in obj && obj.svg) {
+        t = obj.text;
+        if (t[1] > 0) {
+            tobj = document.createElementNS(svgns, "text");
+            var textNode = document.createTextNode(t[0])
+            // position multiplicity
+            tobj.setAttributeNS(null,"x",obj.x[0]);
+            tobj.setAttributeNS(null,"y",obj.y[0]);
+            tobj.setAttributeNS(null,"text-anchor","middle");
+            tobj.setAttributeNS(null,"dominant-baseline","middle");        
+            tobj.setAttributeNS(null,"font-size","24");
+            obj.textobj = tobj;
+            tobj.appendChild(textNode);
+            svg.appendChild(tobj);
+        } else {
+            if (textobj in obj) {
+                svg.removeChild(obj.textobj);
+            }
+        }
     }
 }
 
+function remap(m, k) {
+    return (k in m) ? m[k] : k 
+}
 
-// this takes svg, not inter
-function push(obj, key, value){
+function push(obj, key){
     if (obj.svg) {
-        kind = obj.kind;
+        o = obj[key]
+        kind = obj.kind[0];
+
+        
         if (kind == "circle") {
-            if (key == "x") {
-                key = "cx";
-            }
-            if (key == "y") {
-                key = "cy";
-            }        
-        }
-        if (kind == "line") {
-            if (key == "x") {
-                key = "x1";
-            }
-            if (key == "y") {
-                key = "y1";
-            }        
+            key = remap({"x":"cx", "y":"cy"},key)
         }
         
-        switch (key){
-        case "text":
-            break;
-        default:
-            obj.svg.setAttributeNS(null,key,value)
+        if (kind == "line") {
+            key = remap({"x":"x1", "y":"x2"},key)
+        }
+        
+        if (o[1] > 0) {
+            switch (key){
+            case "text":
+                break;
+            default:
+                obj.svg.setAttributeNS(null,key,o[0], kind)
+            }
         }
     }
 }
 
-function set(obj, k, val) {
-    obj[k] = val;
-    if ("obj" in obj) {
-        push(obj, k, val);
-        push_text(obj, k, val);        
-    }
-}
 
 function create(obj) {
-    if ((!obj.svg) && (obj.kind)) {
-        let o = document.createElementNS(svgns, obj.kind);
-        o.setAttributeNS(null, "obj", obj);
-        obj.svg=o;        
-        for (var key in obj){
-            push(obj, key, obj[key]);
-        }
+    if ("kind" in obj) {
+        k = obj.kind;
+        if ((!obj.svg) && (k[1] > 0)) {
+            let o = document.createElementNS(svgns, k[0]);
+            // svg doesn't seem to be upset if we put junk in here
+            o.setAttributeNS(null, "obj", obj);
+            o.addEventListener("click",
+                               function (evt) {putBatch(x.click)})
         
-        svg.appendChild(o);
-        for (var key in obj){
-            push_text(obj, key, obj[key]);
-        }                                        
+            obj.svg=o;        
+            for (var key in obj){
+                push(obj, key)
+            }
+            svg.appendChild(o);
+            // stacking order - i suppose if we know this is a graph
+            // the line segments can be under
+            push_text(obj);
+        }
+        if ((obj in svg) && (k[1] <= 0)) {
+            svg.removeChild(obj.svg);        
+            delete obj.svg
+        }
     }
 }
 
@@ -101,6 +129,7 @@ function websocket(url) {
         socket = new WebSocket(url)
         
         socket.onopen = function(evt){
+            connWait = 0
             clear()
         }
         
@@ -111,69 +140,56 @@ function websocket(url) {
                 for (const fact of value){
                     let f = fact[0];
                     let w = fact[1];
-
                     if (!(f.u in nodes)) {
-                        console.log("insert", f.u)
                         nodes[f.u] = {}
                     }
                     let obj = nodes[f.u];
 
-                    console.log("prop", f.u, key)
                     switch(key){
                     case "display::Kind":
-                        if (w > 0) {
-                            set(obj, "kind", f.kind)                            
-                        } else {
-                            if (obj.textobj) {
-                                svg.removeChild(obj.textobj);
-                            }
-                            if (obj.svg) {
-                                svg.removeChild(obj.svg);
-                            }
-                        }
+                        update(obj, "kind", f.kind, w)
                         break;
                     case "display::Position":
-                        set(obj, "x", f.x)
-                        set(obj, "y", f.y)                        
+                        update(obj, "x", f.x, w)
+                        update(obj, "y", f.y, w)
                         break;
                     case "display::NextPosition":
-                        set(obj, "x2", f.x)
-                        set(obj, "y2", f.y)                        
+                        update(obj, "x2", f.x, w)
+                        update(obj, "y2", f.y, w)
                         break;                        
                     case "display::Color":
-                        set(obj, "fill", f.color)                    
+                        update(obj, "fill", f.color, w) 
                         break;
                     case "display::Border":
-                        set(obj, "stroke", f.color)                    
+                        update(obj, "stroke", f.color, w)                    
                         break;
                     case "display::Opacity":
-                        set(obj, "opacity", f.opacity)                    
+                        update(obj, "opacity", f.opacity, w)                    
                         break;                                                
                     case "display::Width":
-                        set(obj, "stroke-width", f.width)                    
+                        update(obj, "stroke-width", f.width, w)                    
                         break;                        
                     case "display::Text":
-                        set(obj, "text", f.text)                                        
+                        update(obj, "text", f.text, w)                                        
 	                break;
                     case "display::Radius":
-                        set(obj, "r",f.r)                                        
+                        update(obj, "r", f.r, w)                                        
                     }
                 }
             }
             for (var k in nodes){
-                console.log("digested", k)
-            }
-            for (var k in nodes){
-                create(nodes[k]);
+                n = nodes[k]
+                if (("x" in n) && ("y" in n) && ("kind" in n) && !("svg" in n))
+                    create(n)
             }
         }
         
         socket.onclose = 
             function(evt){
 	        svg.setAttributeNS(null, "fill", "grey") 
-	        connWait = connWait * 2 + 1000
-	        if (connWait > 5000) {
-		    connWait = 5000
+	        connWait = connWait * 2 + 100
+	        if (connWait > 2000) {
+		    connWait = 2000
 	        }
 	        websocket(url)
 	    }
